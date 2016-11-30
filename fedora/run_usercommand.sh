2dce71ce3b81236c500435c44d5659ddf97625b8
Gerard Braad <me@gbraad.nl>
2016-11-30 03:03:39 +0000

    Prevent LD_PRELOAD error

--- fedora/run_usercommand.sh
+++ fedora/run_usercommand.sh
@@ -2,7 +2,7 @@
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /tmp/passwd_template > /tmp/passwd
-export LD_PRELOAD=libnss_wrapper.so
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
